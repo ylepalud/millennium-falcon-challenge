6ec386489142bfd59ad8dc05d22d80f6f4ee1d9c
Yves Le Palud <yves.le-palud-extern@renault.com>
2024-06-07 02:38:14 +0200

    Compute BountyHunterCapture cost function

---
+++ libs/solver/src/main/java/org/example/domain/cost/function/BountyHunterCapture.java
@@ -0,0 +1,21 @@
+package org.example.domain.cost.function;
+
+public class BountyHunterCapture {
+
+  static double costFunction(int hit) {
+    if (hit == 0) {
+      return 0.0;
+    }
+
+    if (hit == 1) {
+      return 0.1;
+    }
+
+    var upperPart = Math.pow(9, hit - 1);
+    var downPart = Math.pow(10, hit);
+
+    var partialResult = upperPart / downPart;
+
+    return costFunction(hit - 1) + partialResult;
+  }
+}
