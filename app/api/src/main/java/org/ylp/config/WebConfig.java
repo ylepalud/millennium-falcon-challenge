8b7b5de9af7ea5ed2f566f640c776672e3ad7f15
Yves Le Palud <yves.le-palud-extern@renault.com>
2024-06-10 14:57:45 +0200

    Spotless apply

--- app/api/src/main/java/org/ylp/config/WebConfig.java
+++ app/api/src/main/java/org/ylp/config/WebConfig.java
@@ -6,12 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
 }
